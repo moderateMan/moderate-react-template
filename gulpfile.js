332d18e35cc9e1aebb9cc0fdb62adafe616bb7f9
qanglee <qanglee123@163.com>
2021-07-06 21:38:52 +0800

    feat: gulp结合git

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,62 @@
-const { series, parallel, src, dest } = require('gulp');
-var del = require('del');
+const { series, parallel, src, dest } = require("gulp");
+var exec = require("child_process").exec;
+var del = require("del");
 
 function cleanGame(cb) {
-    return del([
-        'build/web-mobile',
-        // 这里我们使用一个通配模式来匹配 `mobile` 文件夹中的所有东西
-    ]).then(() => {
-        cb()
-        process.exit();
-    });
+  return del([
+    "build/web-mobile",
+    // 这里我们使用一个通配模式来匹配 `mobile` 文件夹中的所有东西
+  ]).then(() => {
+    cb();
+    process.exit();
+  });
 }
 
 // 删除本地调试的引擎文件
 function delDevCocosJs(cb) {
-    return del([
-        'build/web-mobile/cocos2d-js.js',
-    ]).then(() => {
-        cb()
-    });
+  return del(["build/web-mobile/cocos2d-js.js"]).then(() => {
+    cb();
+  });
 }
 
 // 拷贝发布版本的引擎文件
 function copyProductCocosJs() {
-    return src('src/assets/cocos2d-js.js').pipe(dest('build/web-mobile/'));
+  return src("src/assets/cocos2d-js.js").pipe(dest("build/web-mobile/"));
 }
 
 function end(cb) {
-    cb();
-    process.exit(0)
+  cb();
+  process.exit(0);
 }
 
+function cleanGame(cb) {
+  return del([
+    "build/web-mobile",
+    // 这里我们使用一个通配模式来匹配 `mobile` 文件夹中的所有东西
+  ]).then(() => {
+    cb();
+    process.exit();
+  });
+}
+
+
+function githubPull(cb) {
+    return exec("git pull", function (err, stdout, stderr) {
+        cb(err);
+      });
+  }
+
+  function giteePull(cb) {
+    return exec("git pull gitee", function (err, stdout, stderr) {
+        cb(err);
+      });
+  }
 
 // electron任务
 exports.delDevCocosJs = delDevCocosJs;
 // electron任务
-exports.processGame = series(delDevCocosJs,copyProductCocosJs, end);
+exports.processGame = series(delDevCocosJs, copyProductCocosJs, end);
+exports.pull = series(githubPull, giteePull, end);
 
 // 正常打包的任务
 exports.cleanGame = cleanGame;
