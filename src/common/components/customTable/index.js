45586c94076ad6c5cf5a2a21256d23dcaa9450b9
qangLee <545540710@qq.com>
2021-08-10 16:36:29 +0800

    feat: 去除不必要的代码

--- src/common/components/customTable/index.js
+++ src/common/components/customTable/index.js
@@ -403,37 +403,3 @@ class CustomTable extends React.Component {
 }
 
 export default (CustomTable);
-
-
-async function async1() {
-  console.log('async1 start');
-  await async2();
-  console.log('async1 end');
-}
-async function async2() {
-  console.log('async2');
-}
-console.log('script start');
-setTimeout(function() {
-  console.log('setTimeout');
-}, 0)
-async1();
-new Promise(function(resolve) {
-  console.log('promise1');
-  resolve();
-}).then(function() {
-  console.log('promise2');
-});
-console.log('script end');
-
-1
-'script start'
-'async1 start'
-'async2'
-'promise1'
-'script end'
-
-2
-'async1 end'
-'promise2'
-'setTimeout'
