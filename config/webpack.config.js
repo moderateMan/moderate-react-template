8d6c69fc354aa1059bc82454d49896690a32b63e
qangLee <545540710@qq.com>
2021-08-25 16:34:09 +0800

    test

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -844,8 +844,8 @@ module.exports = function (webpackEnv) {
         // Turn off performance processing because we utilize
         // our own hints via the FileSizeReporter
         performance: false,
-        externals: (isEnvProduction && !isEnvElectron) ? {
-            mockjs: mockjs
-        } : {}
+        // externals: (isEnvProduction && !isEnvElectron) ? {
+        //     mockjs: mockjs
+        // } : {}
     };
 };
