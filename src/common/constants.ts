edb909dd4969bd394fdb607a8fb17f4de831624e
victor <victorfm@163.com>
2021-10-15 20:09:45 +0800

    modify a little

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -6,8 +6,7 @@ export const USER_NAME = "userName"; //用户名缓存
 export const IS_SIMPLE = "isSimple"; //密码缓存
 export const MENU = "MENU"; // 菜单缓存
 export const BASE_ROUTER_URL = "/pageCenter";
-export const DATA_MODIFY_WARNING =
-    "该行数据发生过修改，请通过“保存”按钮进行操作！";
+export const DATA_MODIFY_WARNING = "该行数据发生过修改，请通过“保存”按钮进行操作！";
 export const TIMEOUT_INFO = "请求超时，请稍后重试！";
 export const EXCEPTION_INFO = "服务器异常，请联系管理员！";
 export const INFO = "info";
