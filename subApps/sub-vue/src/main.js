3aa8bd806cc4dbf2ca8c08df7c2913b8ce51ae9b
qangLee <545540710@qq.com>
2021-11-30 14:11:58 +0800

    feat: test ci/cd

--- subApps/sub-vue/src/main.js
+++ subApps/sub-vue/src/main.js
@@ -2,7 +2,7 @@ import './public-path'
 import Vue from 'vue'
 import App from './App.vue'
 import routes from './router'
-import { store as commonStore } from 'common'
+// import { store as commonStore } from 'common'
 import store from './store'
 import VueRouter from 'vue-router'
 
@@ -42,7 +42,7 @@ if (!window.__POWERED_BY_QIANKUN__) {
   // 这里是子应用独立运行的环境，实现子应用的登录逻辑
 
   // 独立运行时，也注册一个名为global的store module
-  commonStore.globalRegister(store)
+  // commonStore.globalRegister(store)
   // 模拟登录后，存储用户信息到global module
   const userInfo = { name: '我是独立运行时名字叫张三' } // 假设登录后取到的用户信息
   store.commit('global/setGlobalState', { user: userInfo })
@@ -57,7 +57,7 @@ export async function bootstrap () {
 export async function mount (props) {
   console.log('[vue] props from main framework', props)
 
-  commonStore.globalRegister(store, props)
+  // commonStore.globalRegister(store, props)
 
   render(props)
 }
--- subApps/sub-vue/vue.config.js
+++ subApps/sub-vue/vue.config.js
@@ -2,7 +2,6 @@ const { name } = require("./package.json")
 
 module.exports = {
   publicPath: '/subapp/sub-vue',
-  transpileDependencies: ['common'],
   chainWebpack: config => config.resolve.symlinks(false),
   configureWebpack: {
     output: {
